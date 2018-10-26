ea15ba24468b705905f4fd393b2ccd56d7a5f2a0
Domas Bitvinskas <domas.bitvinskas@me.com>
2018-10-26 13:55:49 +0000

    Cleaner formula

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ import maxWidth from './lib/maxWidth';
 import numberParser from 'number-parsing';
 
 export default (min, max) => {
-  return `calc(${min}px + (${max} - ${min}) * ((100vw - ${minWidth}) / (${numberParser(maxWidth)} - ${numberParser(minWidth)})))`;
+  return `calc(
+    ${min}px +
+    (${max} - ${min}) *
+    (
+      (100vw - ${minWidth}) /
+      (${numberParser(maxWidth)} - ${numberParser(minWidth)})
+    )
+  )`;
 };
